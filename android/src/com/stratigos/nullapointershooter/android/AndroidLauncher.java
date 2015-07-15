031d0563ed61c4929a979700ccca3fbdf5112c45
Stratigos <Stratigos@users.noreply.github.com>
2015-07-15 12:26:08 -0400

    Pluralsight 'NullaPointerShooter' game: initial commit, after creating emulator and adjusting Manifest file for backwards compatibility;

---
+++ android/src/com/stratigos/nullapointershooter/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.stratigos.nullapointershooter.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.stratigos.nullapointershooter.ShooterGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ShooterGame(), config);
+	}
+}
---
+++ desktop/src/com/stratigos/nullapointershooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stratigos.nullapointershooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stratigos.nullapointershooter.ShooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShooterGame(), config);
+	}
+}
